d4c9ffdb6f9038d8ea2bd27738c64dd6730b06d1
Niels Mandrus <nmandrus1@gmail.com>
2024-07-04 22:18:48 -0400

    Enhance CPU emulation with execution logic and memory handling improvements
    This commit introduces significant enhancements to the CPU struct and its associated methods in our CHip 8 emulator. Key changes include:
    
    1. Addition of the `i` register to the CPU struct for storing addresses.
    2. Refactoring of the `decode` method into `decode_and_execute`, integrating instruction execution logic directly within the decoding process. This streamlines the emulation flow by combining decoding and execution steps, improving the readability and maintainability of the code.
    3. Implementation of comprehensive decoding and execution for a variety of instructions such as conditional skips, arithmetic operations, and memory interactions. This includes handling of carry and borrow flags where appropriate.
    4. Optimization of conditions for skipping instructions, using logical operators to merge similar operations and reduce redundancy.
    5. Revision of the main program loop to utilize a memory poller for input handling, enhancing the emulator's interaction capabilities.
    6. Updates to `.gitignore` to include `zig-out` directory, ensuring build artifacts are excluded from version control.
    
    Additionally, several tests were updated to align with the new `decode_and_execute` method, ensuring all unit tests reflect the updated operational logic of the emulator.
    
    These updates collectively improve the emulator's accuracy in replicating the CHip 8 architecture and enhance its efficiency and readability.

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,7 @@ const CPU = struct {
     // program counter & stack ptr
     pc: u12, // program start is 0x200
     sp: u8, // starts at 0x200 and grows down
+    i: u12,
 
     registers: [16]u8,
 
@@ -53,8 +54,12 @@ const CPU = struct {
         return msb | lsb;
     }
 
-    fn decode(self: *CPU, instr: u16) void {
+    fn decode_and_execute(self: *CPU, instr: u16) void {
         const nibble = instr & 0xF000;
+        const x = (instr & 0x0F00) >> 8;
+        const y = (instr & 0x00F0) >> 4;
+        const kk = instr & 0x00FF;
+        const nnn = instr & 0x0FFF;
         switch (nibble) {
             0x0000 => {
                 switch (instr) {
@@ -76,49 +81,87 @@ const CPU = struct {
             },
 
             // 3xkk - SE Vx, byte
-            0x3000 => {
-                const x = instr & 0x0F00;
-                const kk = instr & 0x00FF;
-
-                if (self.registers[x] == kk) {
-                    self.pc += 2;
-                }
-            },
-
             // 4xkk - SNE Vx, byte
-            0x4000 => {
-                const x = instr & 0x0F00;
-                const kk = instr & 0x00FF;
-
-                if (self.registers[x] != kk) {
+            0x3000, 0x4000 => {
+                // conditions to skip instructions for both SE and SNE
+                if ((self.registers[x] == kk and nibble == 0x3000) or
+                    (self.registers[x] != kk and nibble == 0x4000))
                     self.pc += 2;
-                }
             },
 
             // 5xy0 - SE Vx, Vy
-            0x5000 => {
-                const x = instr & 0x0F00;
-                const y = instr & 0x00F0;
-
-                if (self.registers[x] == self.registers[y]) {
+            // 9xy0 - SNE Vx, Vy
+            0x5000, 0x9000 => {
+                // conditions to skip next instr if either are true
+                if ((self.registers[x] == self.registers[y] and nibble == 0x5000) or
+                    (self.registers[x] != self.registers[y] and nibble == 0x9000))
                     self.pc += 2;
-                }
             },
 
             // 6xkk - LD Vx, byte
-            0x6000 => {
-                const x = instr & 0x0F00;
-                const kk = instr & 0x00FF;
-                self.registers[x] = @truncate(kk);
+            0x6000 => self.registers[x] = @truncate(kk),
+
+            // 7xkk - ADD Vx, byte
+            0x7000 => self.registers[x] += @truncate(kk),
+
+            0x8000 => {
+                const lsb = instr & 0x000F;
+                switch (lsb) {
+                    // 0x8xy0 - LD Vx, Vy
+                    0x0 => self.registers[x] = self.registers[y],
+                    // 0x8xy1 - OR Vx, Vy
+                    0x1 => self.registers[x] |= self.registers[y],
+                    // 0x8xy2 - AND Vx, Vy
+                    0x2 => self.registers[x] &= self.registers[y],
+                    // 0x8xy3 - XOR Vx, Vy
+                    0x3 => self.registers[x] ^= self.registers[y],
+                    // 0x8xy4 - ADD Vx, Vy setting carry flag if neccessary
+                    0x4 => {},
+                    // 0x8xy5 - SUB Vx, Vy setting carry flag to NOT overflow
+                    0x5 => {
+                        // Vx = Vx - Vy
+                        // If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
+                        const result = @subWithOverflow(self.registers[x], self.registers[y]);
+                        self.registers[x] = result.@"0";
+                        self.registers[0xF] = ~result.@"1";
+                    },
+
+                    // 0x8xy6 - SHR Vx
+                    0x6 => {
+                        // Vx = Vx >> 1 and Vf is set to 1 if lsb(Vx) = 1
+                        self.registers[0xF] = if (self.registers[x] & 0x01) 1 else 0;
+                        self.registers[x] >> 1;
+                    },
+                    // 8xy7 - SUBN Vx, Vy
+                    0x7 => {
+                        // Set Vx = Vy - Vx, set VF = NOT borrow.
+                        const result = @subWithOverflow(self.registers[y], self.registers[x]);
+                        self.registers[y] = result.@"0";
+                        self.registers[0xF] = ~result.@"1";
+                    },
+                    // 8xyE - SHL Vx {, Vy}
+                    0xE => {
+                        // Set Vx = Vx SHL 1.
+                        self.registers[0xF] = if (self.registers[x] >> 7) 1 else 0;
+                        self.registers[x] << 1;
+                    },
+                    else => unreachable,
+                }
             },
 
-            0x7000 => {},
+            // Annn - LD I, addr
+            0xA000 => self.i = nnn,
 
-            0x8000 => {},
+            // Bnnn - JP V0, addr
+            0xB000 => self.pc = self.registers[0] + nnn,
 
-            0x9000 => {},
+            // Cxkk - RND Vx, byte
+            0xC000 => {
+                const random_byte = std.crypto.random.int(u8);
+                self.registers[x] = random_byte & kk;
+            },
 
-            0xA000 => {},
+            // Dxyn - Draw to screen
 
             else => std.process.exit(1),
         }
@@ -134,16 +177,17 @@ pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const stdin = std.io.getStdIn();
+    // const poller = std.io.Poller(std.fs.File);
+    var poller = std.io.poll(allocator, enum { stdin }, .{ .stdin = stdin });
+    defer poller.deinit();
 
-    try bw.flush(); // don't forget to flush!
+    while (try poller.poll()) {
+        std.debug.print("{any}", .{poller.fifo(.stdin).readItem().?});
+    }
 }
 
 test "stack_push and stack_pop" {
@@ -178,7 +222,7 @@ test "CLS instruction" {
     cpu.memory[0x200] = 0x00;
     cpu.memory[0x201] = 0xE0;
 
-    cpu.decode(cpu.fetch());
+    cpu.decode_and_execute(cpu.fetch());
 
     // No specific state change to check for CLS, just ensure no crash
     try std.testing.expectEqual(@as(u12, 0x201), cpu.pc);
@@ -190,7 +234,7 @@ test "RET instruction" {
     cpu.memory[0x201] = 0xEE;
     cpu.stack_push(0x300);
 
-    cpu.decode(cpu.fetch());
+    cpu.decode_and_execute(cpu.fetch());
 
     try std.testing.expectEqual(0x300, cpu.pc);
     try std.testing.expectEqual(0, cpu.sp);
@@ -200,7 +244,7 @@ test "JP instruction" {
     var cpu = CPU.init();
     cpu.memory[0x200] = 0x12;
 
-    cpu.decode(cpu.fetch());
+    cpu.decode_and_execute(cpu.fetch());
 
     try std.testing.expectEqual(@as(u12, 0x12), cpu.pc);
 }
@@ -210,7 +254,7 @@ test "CALL instruction" {
     cpu.memory[0x200] = 0x22;
     cpu.memory[0x201] = 0x22;
 
-    cpu.decode(cpu.fetch());
+    cpu.decode_and_execute(cpu.fetch());
 
     try std.testing.expectEqual(@as(u12, 0x222), cpu.pc);
     try std.testing.expectEqual(@as(u8, 0x02), cpu.memory[0]);
@@ -224,9 +268,11 @@ test "SE instruction" {
     cpu.memory[0x201] = 0x00;
     cpu.registers[0xA] = 0x00;
 
-    cpu.decode(cpu.fetch());
+    const instr = cpu.fetch();
+    try std.testing.expectEqual(instr, 0x3A00);
+    cpu.decode_and_execute(instr);
 
-    try std.testing.expectEqual(@as(u12, 0x202), cpu.pc);
+    try std.testing.expectEqual(@as(u12, 0x204), cpu.pc);
 }
 
 test "SNE instruction" {
@@ -235,7 +281,7 @@ test "SNE instruction" {
     cpu.memory[0x201] = 0x01;
     cpu.registers[0xA] = 0x00;
 
-    cpu.decode(cpu.fetch());
+    cpu.decode_and_execute(cpu.fetch());
 
     try std.testing.expectEqual(@as(u12, 0x202), cpu.pc);
 }
@@ -247,7 +293,7 @@ test "SE register instruction" {
     cpu.registers[0xA] = 0x01;
     cpu.registers[0xB] = 0x01;
 
-    cpu.decode(cpu.fetch());
+    cpu.decode_and_execute(cpu.fetch());
 
     try std.testing.expectEqual(@as(u12, 0x202), cpu.pc);
 }
@@ -257,7 +303,15 @@ test "LD instruction" {
     cpu.memory[0x200] = 0x6A;
     cpu.memory[0x201] = 0x01;
 
-    cpu.decode(cpu.fetch());
+    cpu.decode_and_execute(cpu.fetch());
 
     try std.testing.expectEqual(@as(u8, 0x01), cpu.registers[0xA]);
+}
+
+test "sub with overflow" {
+    const a: u8 = 1;
+    const b: u8 = 3;
+    const result = @subWithOverflow(a, b);
+    try std.testing.expectEqual(1, result.@"0");
+    try std.testing.expectEqual(253, result.@"0");
 }
